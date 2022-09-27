093af4e8c5d4a889b42c454911bb84be6deb933e
Adolfo Herrera <valherdolf@gmail.com>
2022-09-27 19:16:16 -0400

    Add h2 titles to each section

--- src/components/CV/CV.js
+++ src/components/CV/CV.js
@@ -5,8 +5,11 @@ import ExperienceInfo from './ExperienceInfo/ExperienceInfo';
 
 const CV = () => (
   <>
+    <h2>Personal Information</h2>
     <PersonalInfo />
+    <h2>Education</h2>
     <EducationInfo />
+    <h2>Experience</h2>
     <ExperienceInfo />
   </>
 );
